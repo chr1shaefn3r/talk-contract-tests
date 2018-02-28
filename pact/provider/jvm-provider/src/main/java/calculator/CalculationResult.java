06fd96c9f81e0732c420dd241e0607778734034f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-02-28 22:57:55 +0100

    provider adds a new property that consumer can not parse

--- pact/provider/jvm-provider/src/main/java/calculator/CalculationResult.java
+++ pact/provider/jvm-provider/src/main/java/calculator/CalculationResult.java
@@ -5,4 +5,5 @@ import com.google.gson.annotations.JsonAdapter;
 public class CalculationResult {
 	@JsonAdapter(StripTrailingZerosAfterTheDot.class)
 	public Number result;
+	public String constant = "value";
 }
